585ec612f98effd91f721d7246a2f0b7ab9344f9
Goran <goranlolic2@gmail.com>
2018-11-15 11:43:51 +0100

    test commit

--- AngularCaritas/src/app/app.component.ts
+++ AngularCaritas/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, ViewEncapsulation } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'AngularCaritas';
