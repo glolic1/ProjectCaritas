62092c52af024d0122af2021cdaf9f367f252c7d
Goran <goranlolic2@gmail.com>
2018-12-03 16:28:37 +0100

    Ispravljen sort i paging

--- AngularCaritas/src/app/residence/residence.component.ts
+++ AngularCaritas/src/app/residence/residence.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, AfterViewInit } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource, MatButton, MatTab } from '@angular/material';
 
   export interface Korisnik {
@@ -18,7 +18,7 @@ import { MatPaginator, MatSort, MatTableDataSource, MatButton, MatTab } from '@a
     {naziv:'Crnomerec',adresa:'Ilica 200', opis:'stan'},
     {naziv:'Maksimir',adresa:'Maksimirska 200', opis:'stan'},
     {naziv:'Dubrava',adresa:'Ravnice 200', opis:'kuca'},
-    {naziv:'Dugave',adresa:'Vatikanska 200', opis:'stan'},
+    {naziv:'Dugave',adresa:'Vatikanska 200', opis:'stan'}
     
   ];
   const ELEMENT_DATA_SOBA: Soba[] =[
@@ -34,25 +34,44 @@ import { MatPaginator, MatSort, MatTableDataSource, MatButton, MatTab } from '@a
   templateUrl: './residence.component.html',
   styleUrls: ['./residence.component.css']
 })
-export class ResidenceComponent implements OnInit {
+export class ResidenceComponent implements OnInit, AfterViewInit {
   // displayedColumns: string[] = ['oib','name','lastName','email','pbr'];
   // dataSource = new MatTableDataSource(ELEMENT_DATA);
   // @ViewChild(MatPaginator) paginator:MatPaginator;
   // @ViewChild(MatSort) sort:MatSort;
    constructor() { }
    ngOnInit() {
-     this.dataSourceSmjestaj.paginator=this.paginatorSmjestaj;
-     this.dataSourceSmjestaj.sort=this.sortSmjestaj;
-    
-     this.dataSourceSoba.paginator=this.paginatorSoba;
-     this.dataSourceSoba.sort=this.sortSoba;
-
-     this.dataSourceKrevet.paginator=this.paginatorKrevet;
-     this.dataSourceKrevet.sort=this.sortKrevet;
+     
    }
+   ngAfterViewInit(){
+    this.dataSourceSmjestaj.paginator=this.paginatorSmjestaj;
+    this.dataSourceSmjestaj.sort=this.sortSmjestaj;
+   
+    this.dataSourceSoba.paginator=this.paginatorSoba;
+    this.dataSourceSoba.sort=this.sortSoba;
+
+    this.dataSourceKrevet.paginator=this.paginatorKrevet;
+    this.dataSourceKrevet.sort=this.sortKrevet;
+   }
+
+   _setDataSource(indexNumber:number){
+     setTimeout(()=>{
+       switch(indexNumber){
+         case 0:
+          !this.dataSourceSmjestaj ? this.dataSourceSmjestaj.paginator=this.paginatorSmjestaj : null;
+          break;
+        case 1:
+          !this.dataSourceSoba.paginator ? this.dataSourceSoba.paginator = this.paginatorSoba : null;
+          break;
+        case 2:
+          !this.dataSourceKrevet.paginator ? this.dataSourceKrevet.paginator = this.paginatorKrevet : null;
+          
+       }
+     });
+   }
+
    applyFilterSmjestaj(filterValue: string) {
       this.dataSourceSmjestaj.filter = filterValue.trim().toLowerCase();
-      
    }
    applyFilterSoba(filterValue:string){
     this.dataSourceSoba.filter=filterValue.trim().toLowerCase();
@@ -65,13 +84,13 @@ export class ResidenceComponent implements OnInit {
   displayedColumnsSmjestaj:string[]= ['naziv','adresa','opis'];
   displayedColumnsSoba:string[] = ['nazivSobe','opisSobe'];
   displayedColumnsKrevet:string[]=['vrstaKrevet','opisKrevet'];
-  dataSourceSmjestaj=new MatTableDataSource(ELEMENT_DATA_SMJESTAJ);
-  dataSourceSoba = new MatTableDataSource(ELEMENT_DATA_SOBA);
-  dataSourceKrevet = new MatTableDataSource(ELEMENT_DATA_KREVET);
-  @ViewChild(MatPaginator) paginatorSmjestaj:MatPaginator;
-  @ViewChild(MatSort) sortSmjestaj:MatSort;
-  @ViewChild(MatPaginator) paginatorSoba:MatPaginator;
-  @ViewChild(MatSort) sortSoba:MatSort;
-  @ViewChild(MatPaginator) paginatorKrevet:MatPaginator;
-  @ViewChild(MatSort) sortKrevet:MatSort;
+  dataSourceSmjestaj=new MatTableDataSource<Korisnik>(ELEMENT_DATA_SMJESTAJ);
+  dataSourceSoba = new MatTableDataSource<Soba>(ELEMENT_DATA_SOBA);
+  dataSourceKrevet = new MatTableDataSource<Krevet>(ELEMENT_DATA_KREVET);
+  @ViewChild('paginator1') paginatorSmjestaj:MatPaginator;
+  @ViewChild('sort1') sortSmjestaj:MatSort;
+  @ViewChild('paginator2') paginatorSoba:MatPaginator;
+  @ViewChild('sort2') sortSoba:MatSort;
+  @ViewChild('paginator3') paginatorKrevet:MatPaginator;
+  @ViewChild('sort3') sortKrevet:MatSort;
 }
