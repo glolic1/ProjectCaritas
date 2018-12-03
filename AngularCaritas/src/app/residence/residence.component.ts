5cfa050b6a25d567f36d9e3894a291ddcb75c009
Goran <goranlolic2@gmail.com>
2018-12-03 12:02:03 +0100

    dodan krevet grid

--- AngularCaritas/src/app/residence/residence.component.ts
+++ AngularCaritas/src/app/residence/residence.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatSort, MatTableDataSource, MatButton } from '@angular/material';
+import { MatPaginator, MatSort, MatTableDataSource, MatButton, MatTab } from '@angular/material';
 
-  export interface User {
+  export interface Korisnik {
     naziv:string;
     adresa:string;
     opis: string;
   }
-  export interface Room{
+  export interface Soba{
     nazivSobe:string;
     opisSobe:string;
   }
-  const ELEMENT_DATA_SMJESTAJ: User[] = [
+  export interface Krevet{
+    vrstaKrevet:string;
+    opisKrevet:string;
+  }
+  const ELEMENT_DATA_SMJESTAJ: Korisnik[] = [
     {naziv:'Crnomerec',adresa:'Ilica 200', opis:'stan'},
     {naziv:'Maksimir',adresa:'Maksimirska 200', opis:'stan'},
     {naziv:'Dubrava',adresa:'Ravnice 200', opis:'kuca'},
     {naziv:'Dugave',adresa:'Vatikanska 200', opis:'stan'},
     
   ];
-  const ELEMENT_DATA_SOBA: Room[] =[
+  const ELEMENT_DATA_SOBA: Soba[] =[
     {nazivSobe:'neka soba', opisSobe:'lijepa soba'},
     {nazivSobe:'jos jedna soba', opisSobe:'ne toliko lijepa soba'}
+  ];
+  const ELEMENT_DATA_KREVET: Krevet[] = [
+    {vrstaKrevet:'obicni krevet', opisKrevet:'obican'},
+    {vrstaKrevet:'neobican krevet', opisKrevet:'neobican'}
   ];
 @Component({
   selector: 'app-residence',
@@ -35,21 +43,35 @@ export class ResidenceComponent implements OnInit {
    ngOnInit() {
      this.dataSourceSmjestaj.paginator=this.paginatorSmjestaj;
      this.dataSourceSmjestaj.sort=this.sortSmjestaj;
-
+    
      this.dataSourceSoba.paginator=this.paginatorSoba;
      this.dataSourceSoba.sort=this.sortSoba;
+
+     this.dataSourceKrevet.paginator=this.paginatorKrevet;
+     this.dataSourceKrevet.sort=this.sortKrevet;
    }
-   applyFilter(filterValue: string) {
+   applyFilterSmjestaj(filterValue: string) {
       this.dataSourceSmjestaj.filter = filterValue.trim().toLowerCase();
-      this.dataSourceSoba.filter=filterValue.trim().toLowerCase();
+      
+   }
+   applyFilterSoba(filterValue:string){
+    this.dataSourceSoba.filter=filterValue.trim().toLowerCase();
+   }
+   applyFilterKrevet(filterValue:string){
+    this.dataSourceKrevet.filter=filterValue.trim().toLowerCase();
+
    }
   
   displayedColumnsSmjestaj:string[]= ['naziv','adresa','opis'];
   displayedColumnsSoba:string[] = ['nazivSobe','opisSobe'];
+  displayedColumnsKrevet:string[]=['vrstaKrevet','opisKrevet'];
   dataSourceSmjestaj=new MatTableDataSource(ELEMENT_DATA_SMJESTAJ);
   dataSourceSoba = new MatTableDataSource(ELEMENT_DATA_SOBA);
+  dataSourceKrevet = new MatTableDataSource(ELEMENT_DATA_KREVET);
   @ViewChild(MatPaginator) paginatorSmjestaj:MatPaginator;
   @ViewChild(MatSort) sortSmjestaj:MatSort;
   @ViewChild(MatPaginator) paginatorSoba:MatPaginator;
   @ViewChild(MatSort) sortSoba:MatSort;
+  @ViewChild(MatPaginator) paginatorKrevet:MatPaginator;
+  @ViewChild(MatSort) sortKrevet:MatSort;
 }
